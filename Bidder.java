481bf13158be91a2b2c61acef6d73dc749e0415f
dloulias <demyloulias@gmail.com>
2015-11-04 18:00:54 -0800

    Create Bidder.java

---
+++ Bidder.java
@@ -0,0 +1,11 @@
+public class Bidder extends User
+{
+  Map<Auction, double> myBids;
+  // need to store credit card info
+  
+  // bids on items
+  // views auctions
+  // chooses auctions
+  // views items
+  // updates credit card info
+}
