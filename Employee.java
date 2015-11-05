beb841393719a20ddbc75d2dde3a008ddbcf8e0e
dloulias <demyloulias@gmail.com>
2015-11-04 17:54:55 -0800

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,15 @@
+public class Employee extends User
+{
+  public void viewCurrentCalendarMonth()
+  {
+  }
+  
+  public void viewSelectedCalendarMonth(String Month)
+  {
+  }
+  
+  public void viewAuctionDetails(Date auction)
+  {
+  }
+  
+}
