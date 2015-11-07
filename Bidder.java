6009028bba1fcf7f10b7a5ee571908dc5f293181
quinncox <qlcforever@hotmail.com>
2015-11-07 15:50:46 -0800

    Update Bidder.java

--- Bidder.java
+++ Bidder.java
@@ -1,7 +1,83 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Bidder extends User
 {
-  Map<Auction, double> myBids;
-  // need to store credit card info
+  private Map<Item, Double> myBids;
+  
+  public Bidder(String theUserName, UserType theUserType)
+  {
+	  super(theUserName, theUserType);
+	  myBids = new HashMap<Item, Double>();
+  }
+  
+  //if fail return false, also print to console why failed.
+  public boolean bid(Item theItem, double theBid){
+	  boolean answer = false;
+	  boolean allDone = false;
+	  if (theItem.getMinimumBid() < theBid) {
+		  allDone = true;
+		  System.out.println("Bid does not meet minimum bid for this item.");
+	  }
+	  if (!allDone) {
+		  myBids.put(theItem, theBid);
+		  answer = true;
+	  }	  
+	return answer;
+  }
+
+@Override
+public ArrayList<Command> ExecuteCommand(Command theCommand, AuctionCalendar theCalendar,
+		Auction theAuction, Item theItem) {
+	ArrayList<Command> answer = new ArrayList<Command>();
+	Scanner user_input = new Scanner( System.in );
+	switch (theCommand) {
+		case VIEWAUCTION:
+			answer.add(User.Command.GOBACK);
+			answer.add(User.Command.VIEWITEM);
+			break;
+		case VIEWCALENDAR:
+			answer.add(User.Command.GOBACK);
+			answer.add(User.Command.VIEWAUCTION);
+			break;
+		case VIEWITEM:
+			answer.add(User.Command.GOBACK);
+			if(myBids.containsKey(theItem)) {
+				answer.add(User.Command.EDITBID);
+			} else {
+				answer.add(User.Command.BID);
+			}				
+			break;
+		case VIEWMAINMENU:
+			answer.add(User.Command.VIEWCALENDAR);
+			break;
+		case BID:
+			bidOnItem(user_input, theItem);
+			break;
+		case EDITBID:
+			System.out.println("This is the previous Bid: $");
+			System.out.println(myBids.get(theItem).toString());
+			bidOnItem(user_input, theItem);
+			break;
+		default:
+			break;
+	}
+	user_input.close();
+	return answer;
+}
+
+private void bidOnItem(Scanner user_input,Item theItem) {
+	double bid;
+	System.out.println("Please Enter a Bid:");
+	bid = user_input.nextDouble();
+	if (theItem.getMinimumBid() > bid) {
+		System.out.println("Bid is below minimum bid for this item.");
+	}else {
+		myBids.put(theItem, bid);
+	}
+}
   
   // bids on items
   // views auctions
