a8f2b128459dd4f45b9a903127705045ccac52bd
quinncox <qlcforever@hotmail.com>
2015-11-16 15:21:48 -0800

    uploaded the tests we did during meeting

--- ItemTest.java
+++ ItemTest.java
@@ -27,6 +27,41 @@ public class ItemTest {
 	public void testItem() {
 		fail("Not yet implemented");
 	}
+	
+	Item itemToTest;
+	Bidder bidderToTest;
+	
+	double bidTooLow = 23;
+	double theNextBid = 41;
+	//User bidderToTest;
+	
+	@Before
+	public void setupBefore()
+	{
+		bidderToTest = new Bidder("brendo", User.UserType.BIDDER);
+	}
+
+	@Test
+	public void testItemBidAddedOneBid() {
+		
+		//User bidderToTest = new User("brendo", User.UserType.BIDDER);
+		Item itemToTest = new Item("Chair", 25, "nice flashy chair");
+		int numBids = itemToTest.getBids().size();
+		itemToTest.bidOnItem(bidderToTest, theNextBid);
+		itemToTest.getBids().get(bidderToTest);
+		assertTrue(itemToTest.getBids().get(bidderToTest).doubleValue() == theNextBid);			
+	}
+	
+	@Test
+	public void testItemBidBidChanged() {
+		
+		//User bidderToTest = new User("brendo", User.UserType.BIDDER);
+		Item itemToTest = new Item("Chair", 25, "nice flashy chair");
+		int numBids = itemToTest.getBids().size();
+		itemToTest.bidOnItem(bidderToTest, theNextBid);
+		assertEquals(numBids + 1, itemToTest.getBids().size());			
+	}
+
 
 	/**
 	 * Test method for {@link Item#getBids()}.
