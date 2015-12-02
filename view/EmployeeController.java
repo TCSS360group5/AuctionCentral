2ae791a2041d38105db0769a510dae48e37214d5
shmurphy <shannonnmurphy3@gmail.com>
2015-12-01 19:58:49 -0800

    EmployeeController tests updated

--- view/EmployeeController.java
+++ view/EmployeeController.java
@@ -7,71 +7,77 @@ import model.UserModel;
 /**
  * This class represents an Auction Central Employee and gives the
  *  functionality to move around the menus.
+ *  
+ *  @author TCSS 360 Group 5
  */
 public class EmployeeController extends UserController
 {
 	
 	private EmployeeModel myEmployeeModel;
 
-  /**
- * @param theUsername
- * @param theUserType
- */
-public EmployeeController(UserModel theModel) {
-		super("Employee");
-		myEmployeeModel = (EmployeeModel) theModel;		
+	/**
+	 * Creates a new EmployeeController using the EmployeeModel passed in.
+	 * 
+	 * @param theModel the EmployeeModel to be used by the controller.
+	 */
+	public EmployeeController(UserModel theModel) {
+			super("Employee");
+			myEmployeeModel = (EmployeeModel) theModel;		
+		}
+	
+	public EmployeeModel getEmployeeModel() {
+		return myEmployeeModel;
 	}
-
-//  public ArrayList<Command> GetMenu(Command theCommand, Item theItem)
-//  {
-//		ArrayList<Command> answer = new ArrayList<Command>();
-//		switch (theCommand) {
-//		case VIEWAUCTIONDETAILS:
-//			answer.add(User.Command.GOBACK);
-//			break;
-//		case VIEWCALENDAR:
-//			answer.add(User.Command.GOBACK);
-//			answer.add(User.Command.VIEWAUCTIONDETAILS);
-//			break;
-//		case VIEWMAINMENU:
-//			answer.add(User.Command.VIEWCALENDAR);
-//			break;
-//		default:
-//			System.out.println("Menu Not Recognized");
-//			break;
-//		}
-//		return answer;
-//  }
-  
-///* (non-Javadoc)
-// * @see User#ExecuteCommand(User.Command, Calendar, Auction, Item)
-// */
-//public boolean ExecuteCommand(Command theCommand, Calendar theCalendar, Auction theAuction, Item theItem)
-//  {
-//	boolean answer = false;
-//	switch (theCommand) {
-//	default:
-//		System.out.println("Command Not Recognized");
-//		break;
-//	}
-//	return answer;
-//  }
-
-//	public User.Command goBackState(User.Command theCurrentState) 
-//	{
-//	  User.Command answer = theCurrentState;
-//		switch (theCurrentState)
-//		 {			
-//			case VIEWAUCTIONDETAILS:
-//				answer = User.Command.VIEWCALENDAR;
-//				break;
-//			case VIEWCALENDAR:
-//				answer = User.Command.VIEWMAINMENU;
-//				break;
-//	 		default:
-//	 			System.out.println("Cannot Go Back");
-//	 			break;						 
-//		 }		
-//		return answer;
-//	}  
+	//  public ArrayList<Command> GetMenu(Command theCommand, Item theItem)
+	//  {
+	//		ArrayList<Command> answer = new ArrayList<Command>();
+	//		switch (theCommand) {
+	//		case VIEWAUCTIONDETAILS:
+	//			answer.add(User.Command.GOBACK);
+	//			break;
+	//		case VIEWCALENDAR:
+	//			answer.add(User.Command.GOBACK);
+	//			answer.add(User.Command.VIEWAUCTIONDETAILS);
+	//			break;
+	//		case VIEWMAINMENU:
+	//			answer.add(User.Command.VIEWCALENDAR);
+	//			break;
+	//		default:
+	//			System.out.println("Menu Not Recognized");
+	//			break;
+	//		}
+	//		return answer;
+	//  }
+	  
+	///* (non-Javadoc)
+	// * @see User#ExecuteCommand(User.Command, Calendar, Auction, Item)
+	// */
+	//public boolean ExecuteCommand(Command theCommand, Calendar theCalendar, Auction theAuction, Item theItem)
+	//  {
+	//	boolean answer = false;
+	//	switch (theCommand) {
+	//	default:
+	//		System.out.println("Command Not Recognized");
+	//		break;
+	//	}
+	//	return answer;
+	//  }
+	
+	//	public User.Command goBackState(User.Command theCurrentState) 
+	//	{
+	//	  User.Command answer = theCurrentState;
+	//		switch (theCurrentState)
+	//		 {			
+	//			case VIEWAUCTIONDETAILS:
+	//				answer = User.Command.VIEWCALENDAR;
+	//				break;
+	//			case VIEWCALENDAR:
+	//				answer = User.Command.VIEWMAINMENU;
+	//				break;
+	//	 		default:
+	//	 			System.out.println("Cannot Go Back");
+	//	 			break;						 
+	//		 }		
+	//		return answer;
+	//	}  
 }
--- view/EmployeeControllerTest.java
+++ view/EmployeeControllerTest.java
@@ -1,18 +1,23 @@
 package view;
 import static org.junit.Assert.*;
+import model.*;
 
 import org.junit.Before;
 import org.junit.Test;
 
 
 public class EmployeeControllerTest {
-
+	
+	EmployeeController myTestEmployee;
+	
 	@Before
 	public void setUp() throws Exception {
+		EmployeeModel employeeModel = new EmployeeModel("Shannon", UserModel.UserType.EMPLOYEE);
+		myTestEmployee = new EmployeeController(employeeModel);
 	}
 
 	@Test
-	public void testEmployee() {
-		fail("Not yet implemented");
+	public void testEmployeeModel() {
+		assertEquals(myTestEmployee.getEmployeeModel().getUserName(), "Shannon");
 	}
 }
