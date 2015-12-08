5a33509582927d9d44bd85db625a3e9530979d90
shmurphy <shannonnmurphy3@gmail.com>
2015-12-07 20:24:44 -0800

    NonProfit view tests updated

--- view/Test/NonProfitControllerTest.java
+++ view/Test/NonProfitControllerTest.java
@@ -1,31 +1,27 @@
 package view.Test;
+import static org.junit.Assert.*;
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 import model.*;
 
 import org.junit.Before;
+import org.junit.Test;
+
+import view.NonProfitController;
 
 public class NonProfitControllerTest {
-NonProfitModel myNPO;
-AuctionModel currentAuction;
-AuctionModel OverAYearAgoAuction;
-AuctionModel AYearAgoAuction;
-AuctionModel LessThanAYearAgoAuction;
-LocalDateTime OverAYearAgo;
-LocalDateTime AYearAgo;
-LocalDateTime LessThanAYearAgo;
+	NonProfitController mytestNPO;
 	
 	@Before
 	public void setUp() throws Exception {
-		myNPO = new NonProfitModel("Billy", UserModel.UserType.NPO, "BAuction", LocalDate.now().minusDays(365));
-		LocalDateTime now = LocalDateTime.now();
-		
-		 OverAYearAgo = now.minusDays(366);
-		 AYearAgo = now.minusDays(365);
-		 LessThanAYearAgo = now.minusDays(364);
-		OverAYearAgoAuction = new AuctionModel(myNPO.getNPOName(), myNPO.getUserName(), OverAYearAgo, OverAYearAgo.plusHours(2));
-		AYearAgoAuction = new AuctionModel(myNPO.getNPOName(), myNPO.getUserName(), AYearAgo, AYearAgo.plusHours(2));;
-		LessThanAYearAgoAuction = new AuctionModel(myNPO.getNPOName(), myNPO.getUserName(), LessThanAYearAgo, LessThanAYearAgo.plusHours(2));;
+		NonProfitModel NPO = new NonProfitModel("Shannon", UserModel.UserType.NPO, "Goodwill", LocalDate.now());
+		mytestNPO = new NonProfitController(NPO);
+	}
+	
+	@Test
+	public void testNonProfitController() {
+		assertEquals(mytestNPO.getNPOModel().getUserName(), "Shannon");
 	}
 }
